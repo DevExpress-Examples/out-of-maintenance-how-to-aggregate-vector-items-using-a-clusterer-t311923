7acf4fd60f44eb311545ac1002dd94562b4c7cda
DevExpressExampleBot <fakeEmail>
2021-07-27 16:57:55 +0300

    Source auto update [skip ci]

--- VB/ClustererSample/MainWindow.xaml.vb
+++ VB/ClustererSample/MainWindow.xaml.vb
@@ -2,15 +2,12 @@
 Imports DevExpress.Xpf.Map
 
 Namespace ClustererSample
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        Private Sub MarkerClusterer_Clustered(ByVal sender As Object, ByVal e As ClusteredEventArgs)
-            map.ZoomToFitLayerItems()
-        End Sub
-    End Class
+	End Class
 End Namespace
